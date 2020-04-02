f681f9be6bb4f720b469fcd8a6d95b23e8d6c27d
Damien ARTIGNAN <damien.artignan@addupsolutions.com>
2020-04-02 22:44:22 +0200

    Format code

--- main/src/index.ts
+++ main/src/index.ts
@@ -1,35 +1,38 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
 
 let win;
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 800, height: 600, transparent: false,
-    webPreferences: { // <--- (1) Additional preferences
+    width: 800,
+    height: 600,
+    transparent: false,
+    webPreferences: {
+      // <--- (1) Additional preferences
       nodeIntegration: false,
-      preload: __dirname + '/preload.js' // <--- (2) Preload script
-    }
+      preload: __dirname + "/preload.js", // <--- (2) Preload script
+    },
   });
 
-  win.loadURL('http://localhost:3000'); // <--- (3) Loading react
+  win.loadURL("http://localhost:3000"); // <--- (3) Loading react
 
   win.webContents.openDevTools();
 
-  win.on('closed', () => {
-    win = null
+  win.on("closed", () => {
+    win = null;
   });
-};
+}
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (win === null) {
-    createWindow()
+    createWindow();
   }
 });
