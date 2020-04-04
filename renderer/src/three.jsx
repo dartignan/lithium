14fda6a077b195a5f6bf2898baff93d2add1c282
Damien ARTIGNAN <damien.artignan@gmail.com>
2020-04-04 14:46:09 +0200

    Simplify scene, add selectable cubes

--- renderer/src/three.jsx
+++ renderer/src/three.jsx
@@ -1,46 +1,54 @@
-import * as THREE from 'three'
-import React, { Suspense, useCallback, useRef, useMemo } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
-import Effects from './effects'
+import * as THREE from "three";
+import React, { useCallback, useRef, useMemo, useState } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
 
 function Swarm({ count, mouse }) {
-  const mesh = useRef()
-  const dummy = useMemo(() => new THREE.Object3D(), [])
+  const mesh = useRef();
+  const dummy = useMemo(() => new THREE.Object3D(), []);
 
   const particles = useMemo(() => {
-    const temp = []
+    const temp = [];
     for (let i = 0; i < count; i++) {
-      const t = Math.random() * 100
-      const factor = 20 + Math.random() * 100
-      const speed = 0.01 + Math.random() / 200
-      const xFactor = -20 + Math.random() * 40
-      const yFactor = -20 + Math.random() * 40
-      const zFactor = -20 + Math.random() * 40
-      temp.push({ t, factor, speed, xFactor, yFactor, zFactor, mx: 0, my: 0 })
+      const t = Math.random() * 100;
+      const factor = 20 + Math.random() * 100;
+      const speed = 0.01 + Math.random() / 200;
+      const xFactor = -20 + Math.random() * 40;
+      const yFactor = -20 + Math.random() * 40;
+      const zFactor = -20 + Math.random() * 40;
+      temp.push({ t, factor, speed, xFactor, yFactor, zFactor, mx: 0, my: 0 });
     }
-    return temp
-  }, [count])
+    return temp;
+  }, [count]);
 
   useFrame(state => {
     particles.forEach((particle, i) => {
-      let { t, factor, speed, xFactor, yFactor, zFactor } = particle
-      t = particle.t += speed / 2
-      const a = Math.cos(t) + Math.sin(t * 1) / 10
-      const b = Math.sin(t) + Math.cos(t * 2) / 10
-      const s = Math.max(1.5, Math.cos(t) * 5)
-      particle.mx += (mouse.current[0] - particle.mx) * 0.02
-      particle.my += (-mouse.current[1] - particle.my) * 0.02
+      let { t, factor, speed, xFactor, yFactor, zFactor } = particle;
+      t = particle.t += speed / 2;
+      const a = Math.cos(t) + Math.sin(t * 1) / 10;
+      const b = Math.sin(t) + Math.cos(t * 2) / 10;
+      const s = Math.max(1.5, Math.cos(t) * 5);
+      particle.mx += (mouse.current[0] - particle.mx) * 0.02;
+      particle.my += (-mouse.current[1] - particle.my) * 0.02;
       dummy.position.set(
-        (particle.mx / 10) * a + xFactor + Math.cos((t / 10) * factor) + (Math.sin(t * 1) * factor) / 10,
-        (particle.my / 10) * b + yFactor + Math.sin((t / 10) * factor) + (Math.cos(t * 2) * factor) / 10,
-        (particle.my / 10) * b + zFactor + Math.cos((t / 10) * factor) + (Math.sin(t * 3) * factor) / 10
-      )
-      dummy.scale.set(s, s, s)
-      dummy.updateMatrix()
-      mesh.current.setMatrixAt(i, dummy.matrix)
-    })
-    mesh.current.instanceMatrix.needsUpdate = true
-  })
+        (particle.mx / 10) * a +
+          xFactor +
+          Math.cos((t / 10) * factor) +
+          (Math.sin(t * 1) * factor) / 10,
+        (particle.my / 10) * b +
+          yFactor +
+          Math.sin((t / 10) * factor) +
+          (Math.cos(t * 2) * factor) / 10,
+        (particle.my / 10) * b +
+          zFactor +
+          Math.cos((t / 10) * factor) +
+          (Math.sin(t * 3) * factor) / 10
+      );
+      dummy.scale.set(s, s, s);
+      dummy.updateMatrix();
+      mesh.current.setMatrixAt(i, dummy.matrix);
+    });
+    mesh.current.instanceMatrix.needsUpdate = true;
+  });
 
   return (
     <>
@@ -49,30 +57,63 @@ function Swarm({ count, mouse }) {
         <meshPhongMaterial attach="material" color="white" />
       </instancedMesh>
     </>
-  )
+  );
+}
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={e => setActive(!active)}
+      onPointerOver={e => setHover(true)}
+      onPointerOut={e => setHover(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial
+        attach="material"
+        color={hovered ? "hotpink" : "orange"}
+      />
+    </mesh>
+  );
 }
 
 export default function ThreeScene() {
-  const mouse = useRef([0, 0])
-  const onMouseMove = useCallback(({ clientX: x, clientY: y }) => (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]), [])
+  const mouse = useRef([0, 0]);
+  const onMouseMove = useCallback(
+    ({ clientX: x, clientY: y }) =>
+      (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]),
+    []
+  );
   return (
-    <div style={{ width: '100%', height: '100%' }} onMouseMove={onMouseMove}>
+    <div style={{ width: "100%", height: "100%" }} onMouseMove={onMouseMove}>
       <Canvas
-        gl={{ alpha: false, antialias: false, logarithmicDepthBuffer: true }}
+        gl={{ alpha: false, antialias: true, logarithmicDepthBuffer: true }}
         camera={{ fov: 75, position: [0, 0, 70] }}
         onCreated={({ gl }) => {
-          gl.setClearColor('grey')
-          gl.toneMapping = THREE.ACESFilmicToneMapping
-          gl.outputEncoding = THREE.sRGBEncoding
-        }}>
+          gl.setClearColor("grey");
+          gl.toneMapping = THREE.ACESFilmicToneMapping;
+          gl.outputEncoding = THREE.sRGBEncoding;
+        }}
+      >
         <ambientLight intensity={1.1} />
         <pointLight position={[100, 100, 100]} intensity={2.2} />
         <pointLight position={[-100, -100, -100]} intensity={5} color="red" />
         <Swarm mouse={mouse} count={150} />
-        <Suspense fallback={null}>
-          <Effects />
-        </Suspense>
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
       </Canvas>
     </div>
-  )
+  );
 }
